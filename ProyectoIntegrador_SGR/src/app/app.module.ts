fe57182ad0197037516d45af2753f08ec651fc4b
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2022-06-14 18:04:59 -0300

    Header en proceso

--- ProyectoIntegrador_SGR/src/app/app.module.ts
+++ ProyectoIntegrador_SGR/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArgProgLogoComponent } from './components/arg-prog-logo/arg-prog-logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArgProgLogoComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
