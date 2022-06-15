21bd180aed27b5214cd6285c7fc9b8fec4e2ed41
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2022-06-14 21:33:33 -0300

    proyecto re hecho por error de bootstrap
    proyecto re hecho por error de bootstrap

--- ProyectoIntegrador_SGR/src/app/app.module.ts
+++ ProyectoIntegrador_SGR/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ArgProgLogoComponent } from './components/arg-prog-logo/arg-prog-logo.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { ArgProgLogoComponent } from './components/arg-prog-logo/arg-prog-logo.component';
 import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArgProgLogoComponent,
     RedesComponent,
+    ArgProgLogoComponent,
     BannerComponent
   ],
   imports: [
