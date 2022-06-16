d441a092d1e9c0b4b312e06e04fc49dd39e250ba
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2022-06-15 21:23:44 -0300

    Arreglado error y About Me agregado

--- ProyectoIntegrador_SGR/src/app/app.module.ts
+++ ProyectoIntegrador_SGR/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { ArgProgLogoComponent } from './components/arg-prog-logo/arg-prog-logo.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     RedesComponent,
     ArgProgLogoComponent,
-    BannerComponent
+    BannerComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
